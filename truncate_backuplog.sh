24ae0bbd29514560443d81ec67420170d9515e1f
David Lin <davidclin@users.noreply.github.com>
2018-10-05 00:07:16 -0700

    Update truncate_backuplog.sh
    fix broken line

--- truncate_backuplog.sh
+++ truncate_backuplog.sh
@@ -6,8 +6,7 @@ today=`date '+%Y-%m-%d-'`;
 PATH=$PATH:/usr/local/bin
 
 echo "Syncing backup.log to S3..."
-aws s3 cp /var/log/backup.log s3://bucket_name/backup_logs/app_name/$
-today$instanceid-backup.log
+aws s3 cp /var/log/backup.log s3://bucket_name/backup_logs/app_name/$today$instanceid-backup.log
 
 
 echo "Truncating /var/log/backup.log"

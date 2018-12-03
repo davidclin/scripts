5c352e33bf7b194a53459d783a67255db94fbf75
Ubuntu <david.lin.ctr@tri.global>
2018-12-03 18:28:17 +0000

    minor update to shell script to include output artifacts

--- s3-get-filter-names.sh
+++ s3-get-filter-names.sh
@@ -2,6 +2,14 @@
 #
 # Prints output of bucket names and associated analytics filter name(s)
 #
+# Example artifact:
+#
+#  Bucket Name             Analytics Filter Name(s)
+#  bucket_name_1           foo test
+#  bucket_name_2           test
+#  ...
+
+
 
 buckets=$(aws s3api list-buckets --query Buckets[].Name --output text)
 

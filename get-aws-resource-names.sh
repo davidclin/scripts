c903540d0cd8ab77f9257338178088eb610765ab
David Lin <davidclin@users.noreply.github.com>
2019-03-08 15:00:34 -0800

    Update get-aws-resource-names.sh
    modify to output in markdown

--- get-aws-resource-names.sh
+++ get-aws-resource-names.sh
@@ -6,11 +6,13 @@
 
 ACCOUNTS=default,other-aws-cli-profile-names-go-here
 
-# Loop through the ACCOUNTS array
+# Loop through the ACCOUNTS array and output in markdown
 for i in $(echo $ACCOUNTS | sed "s/,/ /g")
 do
    echo 'Getting list of IAM users, S3 buckets, and IAM roles for account id' $i
-   echo 'Account id:' $i > $i.txt
+
+   echo '# Account id:' $i > $i.txt
+   echo '<pre>' >> $i.txt
    echo 'IAM UserNames' >> $i.txt
    aws iam list-users --profile $i | grep Users >> $i.txt
 
@@ -19,6 +21,9 @@ do
 
    echo 'IAM roles' >> $i.txt
    aws iam list-roles --profile $i | grep RoleName >> $i.txt
+
+   echo '</pre>' >> $i.txt
+   echo '' >> $i.txt
 done
 
 echo 'done!'

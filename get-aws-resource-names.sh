54f3e9bbdeb7f6c0aee1597ed95b6d19648dcfb6
David Lin <davidclin@users.noreply.github.com>
2019-03-08 16:14:16 -0800

    Update get-aws-resource-names.sh
    update with cosmetic fixes to profile name, account id, and s3 output

--- get-aws-resource-names.sh
+++ get-aws-resource-names.sh
@@ -6,26 +6,34 @@
 #   rm output.md
 #   ls -1 *.txt | while read fn ; do cat "$fn" > output.md; done
 
-ACCOUNTS=default,other-aws-cli-profile-names-go-here
-
-# Loop through the ACCOUNTS array and output in markdown
+# Loop through the ACCOUNTS array
 for i in $(echo $ACCOUNTS | sed "s/,/ /g")
 do
    echo 'Getting list of IAM users, S3 buckets, and IAM roles for account id' $i
 
-   echo '# Account id:' $i > $i.txt
+   echo '# Account Profile:' $i > $i.txt
    echo '<pre>' >> $i.txt
-   echo 'IAM UserNames' >> $i.txt
+
+   echo 'Account ID' >> $i.txt
+   aws sts get-caller-identity --query 'Account' --profile $i --output text >> $i.txt
+
+   echo '' >> $i.txt
+
+   echo 'IAM Users' >> $i.txt
    aws iam list-users --profile $i | grep Users >> $i.txt
 
+   echo '' >> $i.txt
+
    echo 'S3 buckets' >> $i.txt
-   aws s3api list-buckets --profile $i | grep Name >> $i.txt
+   aws s3api list-buckets --profile $i | grep \"Name >> $i.txt
+
+   echo '' >> $i.txt
 
    echo 'IAM roles' >> $i.txt
    aws iam list-roles --profile $i | grep RoleName >> $i.txt
 
    echo '</pre>' >> $i.txt
-   echo '' >> $i.txt
+
 done
 
 echo 'done!'

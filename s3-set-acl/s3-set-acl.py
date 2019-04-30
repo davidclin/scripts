dbf5ca28c6038c8199158a737a1a0400d84f5c17
David Lin <davidclin@users.noreply.github.com>
2019-04-30 14:28:55 -0700

    Create s3-set-acl.py
    add script

---
+++ s3-set-acl/s3-set-acl.py
@@ -0,0 +1,31 @@
+"""
+Set bucket-owner-full-control on S3 objects
+"""
+
+import boto3
+from botocore.exceptions import ClientError, ParamValidationError
+
+counter = 0
+
+key=[
+
+]
+
+
+if __name__ == "__main__":
+    client = boto3.client('s3')
+    bucketname = "miru-us-east-1"
+
+try:
+    for i in key:
+        counter += 1
+        print "Count is: " + str(counter)
+        print ""
+        print "Key is: " + str(i)
+        print ""
+        print client.put_object_acl(Bucket=bucketname,Key=i,ACL='bucket-owner-full-control',)
+        print ""
+        print "------------------"
+        print ""
+except ClientError as e:
+        print("Unexpected error: %s" % e)

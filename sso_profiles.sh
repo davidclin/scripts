3168169ba17973fdef06816b6ad733578c19edfd
David Lin <davidclin@users.noreply.github.com>
2022-09-15 15:52:01 -0700

    Update sso_profiles.sh

--- sso_profiles.sh
+++ sso_profiles.sh
@@ -4,12 +4,11 @@
 AccountID=`aws organizations list-accounts --query 'Accounts[*].{ID:Id}'  | jq -r '.[].ID'`
 for i in $AccountID
 do
-	  echo "[profile $i]" >> sso_pro
-	    echo "sso_start_url = https://tri-sso.awsapps.com/start" >> sso_pro
-	      echo "sso_region = us-east-1" >> sso_pro
-	        echo "sso_role_name = AdministratorAccess" >> sso_pro
-		  echo "sso_account_id = $i" >> sso_pro
-		    echo "source_profile = default" >> sso_pro
-		      echo " " >> sso_pro
-
+  echo "[profile $i]" >> sso_pro
+  echo "sso_start_url = https://tri-sso.awsapps.com/start" >> sso_pro
+  echo "sso_region = us-east-1" >> sso_pro
+  echo "sso_role_name = AdministratorAccess" >> sso_pro
+  echo "sso_account_id = $i" >> sso_pro
+  echo "source_profile = default" >> sso_pro
+  echo " " >> sso_pro
 done
